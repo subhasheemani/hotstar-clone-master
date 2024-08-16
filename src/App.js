a85bec8ccc07822bb39d37d3aefbd694dfb2ac2c
subhasheemani <124122309+subhasheemani@users.noreply.github.com>
2024-08-16 12:11:47 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import  {SideBar}  from './Components/sideBar';
+import Home from './Components/Home';
+function App() {
+  return (
+    <div className="h-screen App ">
+      
+      <Home></Home>
+    </div>
+  );
+}
+
+export default App;
